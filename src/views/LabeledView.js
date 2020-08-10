67e0dd74a36cc5dcfb24e30eb34a1ad5885ec2c1
barisbasmak <barisbasmak@gmail.com>
2020-08-10 15:09:39 +0300

    ids in labeledView changed

--- src/views/LabeledView.js
+++ src/views/LabeledView.js
@@ -36,19 +36,19 @@ class Labeled extends Component {
                 colour="green"
                 documentName={docList[4] ? docList[4].name : null}
                 image={docList[4] ? docList[4].image : null}
-                id="1"
+                id="4"
               />
               <ImageCell
                 colour="red"
                 documentName={docList[8] ? docList[8].name : null}
                 image={docList[8] ? docList[8].image : null}
-                id="0"
+                id="8"
               />
               <ImageCell
                 colour="green"
                 documentName={docList[12] ? docList[12].name : null}
                 image={docList[12] ? docList[12].image : null}
-                id="1"
+                id="12"
               />
 
             </div>
@@ -57,25 +57,25 @@ class Labeled extends Component {
                 colour="red"
                 documentName={docList[1] ? docList[1].name : null}
                 image={docList[1] ? docList[1].image : null}
-                id="0"
+                id="1"
               />
               <ImageCell
                 colour="green"
                 documentName={docList[5] ? docList[5].name : null}
                 image={docList[5] ? docList[5].image : null}
-                id="1"
+                id="5"
               />
               <ImageCell
                 colour="red"
                 documentName={docList[9] ? docList[9].name : null}
                 image={docList[9] ? docList[9].image : null}
-                id="0"
+                id="9"
               />
               <ImageCell
                 colour="green"
                 documentName={docList[13] ? docList[13].name : null}
                 image={docList[13] ? docList[13].image : null}
-                id="1"
+                id="13"
               />
             </div>
             <div class="col-md">
@@ -83,25 +83,25 @@ class Labeled extends Component {
                 colour="red"
                 documentName={docList[2] ? docList[2].name : null}
                 image={docList[2] ? docList[2].image : null}
-                id="0"
+                id="2"
               />
               <ImageCell
                 colour="green"
                 documentName={docList[6] ? docList[6].name : null}
                 image={docList[6] ? docList[6].image : null}
-                id="1"
+                id="6"
               />
               <ImageCell
                 colour="red"
                 documentName={docList[10] ? docList[10].name : null}
                 image={docList[10] ? docList[10].image : null}
-                id="0"
+                id="10"
               />
               <ImageCell
                 colour="green"
                 documentName={docList[14] ? docList[14].name : null}
                 image={docList[14] ? docList[14].image : null}
-                id="1"
+                id="14"
               />
             </div>
             <div class="col-md">
@@ -109,25 +109,25 @@ class Labeled extends Component {
                 colour="red"
                 documentName={docList[3] ? docList[3].name : null}
                 image={docList[3] ? docList[3].image : null}
-                id="0"
+                id="3"
               />
               <ImageCell
                 colour="green"
                 documentName={docList[7] ? docList[7].name : null}
                 image={docList[7] ? docList[7].image : null}
-                id="1"
+                id="7"
               />
               <ImageCell
                 colour="red"
                 documentName={docList[11] ? docList[11].name : null}
                 image={docList[11] ? docList[11].image : null}
-                id="0"
+                id="11"
               />
               <ImageCell
                 colour="green"
                 documentName={docList[15] ? docList[15].name : null}
                 image={docList[15] ? docList[15].image : null}
-                id="1"
+                id="15"
               />
             </div>
             <div class="col-md" style={{ border: "3px solid black", borderRadius: '10px', marginTop: "10px", marginRight: '10px' }}>
